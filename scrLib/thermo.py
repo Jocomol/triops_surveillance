84ccd4b1fc5f318b80890ca0e4a2ad8bfa9e9543
Jocomol <joelmeier08@gmail.com>
2020-05-02 00:00:23 +0200

    changes

--- scrLib/thermo.py
+++ scrLib/thermo.py
@@ -3,6 +3,7 @@ import os
 import sys
 from w1thermsensor import W1ThermSensor
 import datetime
+import subprocess
 
 
 class Thermo(W1ThermSensor):
@@ -13,7 +14,7 @@ class Thermo(W1ThermSensor):
             W1ThermSensor.THERM_SENSOR_DS18B20,
             "00000833e8ff")  # TODO
 
-    def read_measurement(self):
+    def read_measurement(self, lamps):
         meassuered_data = [str(datetime.datetime.now().isoformat())]
         meassuered_temps = (self.get_temperatures([
             self.DEGREES_C,
@@ -22,4 +23,22 @@ class Thermo(W1ThermSensor):
         meassuered_data.append(meassuered_temps[0])
         meassuered_data.append(meassuered_temps[1])
         meassuered_data.append(meassuered_temps[2])
+        if lamps:
+            ## TODO
+            if measurement_temps[0] < 23:
+                color = "blue"
+            elif measurement_temps[0] > 23 && measurement_temps[0] <= 24:
+                color = "aqua"
+            elif measurement_temps[0] > 24 && measurement_temps[0] <= 26:
+                color="green"
+            elif measurement_temps[0] > 26 && measurement_temps[0] <= 27:
+                color="orange"
+            elif measurement_temps[0] > 27:
+                color="red"
+            else:
+                color="purple"
+
+            bashCommand = "hueadm light 2 " + color + " bri=255"
+            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+            output, error = process.communicate()
         return meassuered_data
--- scrLib/wsControl.py
+++ scrLib/wsControl.py
@@ -2,7 +2,7 @@
 from thermo import Thermo
 from dbConnector import DBConnector
 import pytemperature
-import logging
+import sys
 
 
 class Controller():  # Controlls everything and manages the weatherstation
@@ -11,10 +11,9 @@ class Controller():  # Controlls everything and manages the weatherstation
         self.name = "Controller"
         self.thermo = Thermo()
         self.db_connector = DBConnector()
-        self.controller_logger.info("All Objects created")
 
     def main(self):  # Calls all methods and writes results into the database
-        data_array = self.thermo.read_measurement()
+        data_array = self.thermo.read_measurement(len(sys.argv) >= 2 && sys.argv[1]=="-l")
         self.db_connector.database_insert_measurement(data_array)
 
 
