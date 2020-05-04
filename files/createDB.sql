1b6bc33d1577a369ba5c1953bae8dd995cba0fdc
Jocomol <joelmeier08@gmail.com>
2020-05-04 16:31:32 +0200

    updated database script

--- files/createDB.sql
+++ files/createDB.sql
@@ -5,17 +5,3 @@ temperature_C REAL,
 temperature_K REAL,
 temperature_F REAL
 );
-
-CREATE TABLE config (
-ID INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-time_measureInterval_weekday REAL,
-time_measureInterval_month REAL,
-time_measureInterval_day REAL,
-time_measureInterval_hour REAL,
-time_measureInterval_minute REAL,
-time_updateInterval_weekday REAL,
-time_updateInterval_month REAL,
-time_updateInterval_day REAL,
-time_updateInterval_hour REAL,
-time_updateInterval_minute REAL
-);
